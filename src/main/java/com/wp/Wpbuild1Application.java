43a06039321be72db2ebac4837a23d2dbe8b9d72
zhanjian <zhanjian@hiynn.com>
2019-06-14 18:25:33 +0800

    添加项目代码结构

---
+++ src/main/java/com/wp/Wpbuild1Application.java
@@ -0,0 +1,13 @@
+package com.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wpbuild1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wpbuild1Application.class, args);
+	}
+
+}
